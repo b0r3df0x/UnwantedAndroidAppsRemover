274764d8cd923ee5ddca2ee9503159176cb7d888
b0r3df0x <129132351+b0r3df0x@users.noreply.github.com>
2023-06-06 07:33:50 +0200

    Create xiaomibloatremove.sh

---
+++ xiaomibloatremove.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+pm uninstall -k --user 0 com.miui.analytics 
+pm uninstall -k --user 0 com.facebook.system 
+pm uninstall -k --user 0 com.facebook.appmanager
+pm uninstall -k --user 0 com.facebook.services  
+pm uninstall -k --user 0 com.xiaomi.glgm 
+pm uninstall -k --user 0 com.xiaomi.mipicks 
+pm uninstall -k --user 0 com.android.hotwordenrollment.xgoogle 
+pm uninstall -k --user 0 com.android.hotwordenrollment.okgoogle 
+pm uninstall -k --user 0 com.google.android.apps.subscriptions.red
+pm uninstall -k --user 0 com.xiaomi.payment  
+pm uninstall -k --user 0 com.miui.msa.global
+pm uninstall -k --user 0 com.xiaomi.midrop 
+pm uninstall -k --user 0 com.mi.android.globalminusscreen 
